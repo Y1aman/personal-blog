5f2be87c059475e4b0676719df8289c5a77ee37f
Y1aman <150268735+Y1aman@users.noreply.github.com>
2025-11-24 02:55:57 +0300

    Adding first methods to ArticleService class

---
+++ PersonalBlog/Models/Article.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PersonalBlog.Models
+{
+    public class Article
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public DateTime PublishDate { get; set; }
+    }
+}
---
+++ PersonalBlog/Services/ArticleService.cs
@@ -0,0 +1,53 @@
+﻿using PersonalBlog.Models;
+
+namespace PersonalBlog.Services
+{
+    public class ArticleService
+    {
+        private readonly string _path;
+
+        public ArticleService()
+        {
+            _path = Path.Combine(Directory.GetCurrentDirectory(), "articles");
+            EnsureDirectory();
+        }
+
+        private void EnsureDirectory()
+        {
+            if (!Directory.Exists(_path))
+            {
+                Directory.CreateDirectory(_path);
+            }
+        }
+        public List<Article> GetAllArticles()
+        {
+            var articles = new List<Article>();
+            var files = Directory.GetFiles(_path, "*.json");
+            foreach (var file in files)
+            {
+                var json = File.ReadAllText(file);
+                var article = System.Text.Json.JsonSerializer.Deserialize<Article>(json);
+
+                if (article != null)
+                {
+                    articles.Add(article);
+                }
+            }
+            return articles;
+        }
+        public Article? GetArticleById(int id)
+        {
+            var filePath = Path.Combine(_path, $"{id}.json");
+            if (!File.Exists(filePath))
+            {
+                return null;
+            }
+            var json = File.ReadAllText(filePath);
+
+            var article = System.Text.Json.JsonSerializer.Deserialize<Article>(json);
+
+            return article;
+        }
+
+    }
+}
