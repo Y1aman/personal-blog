03ca05423d7cf7670bd8c40cad3dd47461d0f317
Your Name <you@example.com>
2025-11-26 15:35:23 +0300

    Generating Article page

--- PersonalBlog/Pages/Article.cshtml.cs
+++ PersonalBlog/Pages/Article.cshtml.cs
@@ -1,12 +1,31 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using PersonalBlog.Models;
+using PersonalBlog.Services;
 
 namespace PersonalBlog.Pages
 {
     public class ArticleModel : PageModel
     {
-        public void OnGet()
+        private readonly ArticleService _service;
+
+        public Article? Article { get; set; }
+
+        public ArticleModel(ArticleService service)
         {
+            _service = service;
+        }
+
+        public IActionResult OnGet(int id)
+        {
+            Article = _service.GetArticleById(id);
+
+            if (Article == null)
+            {
+                return RedirectToPage("/Index");
+            }
+
+            return Page();
         }
     }
 }
